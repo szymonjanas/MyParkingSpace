1f9ede35eb710ce17640bb4e93fb62f7ecf2b03e
szymonjanas <janas.szymon@outlook.com>
2023-06-04 15:06:32 +0200

    [be][refactor] encapsulate main application
    add ArgvDeserializer
    extract Services to package
    set some const and config

--- Backend/__main__.py
+++ Backend/__main__.py
@@ -1,56 +1,92 @@
 import sys, json, os
 import logging
-import loggingConfig
 import DatabaseConnector as dbService
 import Database
 from flask import Flask
-import ConnectionTestService
-import AdmissionControlService
+from services import ConnectionTest, AdmissionControl
+import config
+import utils
 
-configs = None
-with open("Backend/config.json") as configFile:
-    configs = json.load(configFile)
-    configFile.close()
+class Application:
+    ipAddress = None
+    port = None
 
-hostApi = Flask("MyParkingSpaceServer")
-hostApi.register_blueprint(ConnectionTestService.api_connectionTestService)
-hostApi.register_blueprint(AdmissionControlService.api_admissionControlService)
+    def __init__(self, ipAddress : str = None, port : str = None, logFilePath : str = None):
+        self.setLoggingConfig(logFilePath)
+        
+        self.LOG.info('Hello world!')
 
-def RUN_APPLICATION(hostIpAddress, port, logFilePath : str = None):
-    if not logFilePath:
-        logFilePath = configs["defaults"]["logsFilePath"]
-    with open(logFilePath, "w") as file:
-        pass
-    loggingConfig.loadConfig(logFilePath)
-    LOG = logging.getLogger(__name__)
-    LOG.info('Hello world!')
-    LOG.info("Log file location: {}".format(logFilePath))
-    database = dbService.DatabaseConnector()
-    database.connect(configs["defaults"]["sqliteDatabasePath"]) # TODO add database path option from argv
+        self.initDatabase()
 
-    Database.init_database(Database.DatabaseFacade(database))
+        self.setIpAddress(ipAddress)
+        self.setPort(port)
 
-    if not hostIpAddress:
-        hostIpAddress = "127.0.0.1"
-    LOG.info("Flask IP Address: {}:{}".format(hostIpAddress, port))
-    hostApi.run(
-        host = hostIpAddress,
-        port = port
-    )
+        self.initServer()
 
-    database.disconnect()
+    def __del__(self):
+        if self.database:
+            self.database.disconnect()
+
+    def setLoggingConfig(self, logsFilePath):
+        if not logsFilePath:
+            logsFilePath = config.LOGS_FILE_PATH_DEFAULT
+        with open(logsFilePath, "w") as file:
+            pass
+
+        self.LOG = logging.getLogger(__name__)
+        self.LOG.info("Log file location: {}".format(logsFilePath))
+
+        logging.basicConfig(
+            format=config.LOGGING_FORMAT,
+            handlers=[
+                logging.FileHandler(logsFilePath),
+                logging.StreamHandler()
+            ],
+            level=logging.DEBUG)
+
+    def initDatabase(self):
+        self.database = dbService.DatabaseConnector()
+        self.database.connect(config.SQLITE_DATABASE_PATH_DEFAULT) # TODO add database path option from argv
+        Database.init_database(Database.DatabaseFacade(self.database))
+
+    def setIpAddress(self, ipAddress : str = None):
+        self.ipAddress = ipAddress
+        if not self.ipAddress:
+            self.ipAddress = "127.0.0.1"
+
+    def setPort(self, port : str = None):
+        self.port = port
+        if not port:
+            self.port = "5566"
+
+    def initServer(self):
+        self.flaskServer = Flask(config.SERVER_NAME)
+        self.flaskServer.register_blueprint(ConnectionTest.api_connectionTestService)
+        self.flaskServer.register_blueprint(AdmissionControl.api_admissionControlService)
+
+    def runServer(self):
+        if not self.ipAddress or not self.port:
+            self.LOG.error("IpAddress or Port does not exist!")
+            return
+
+        if not self.flaskServer:
+            self.LOG.error("Flask server has not been initiated!")
+            return
+
+        self.LOG.info("Flask Server run with address: {}:{}".format(self.ipAddress, self.port))
+
+        self.flaskServer.run(
+            host = self.ipAddress,
+            port = self.port
+        )
 
 if __name__ == "__main__":
 
-    ipAddress = None
-    logFilePath = None
-    for i in range(1, len(sys.argv)):
-        if sys.argv[i] == "--ipaddress":
-            ipAddress = sys.argv[i+1]
-            i+=1
-        if sys.argv[i] == "--logfilepath":
-            logFilePath = sys.argv[i+1]
-            i+=1
+    args = utils.ArgvDeserializer(sys.argv)
 
     # TODO custom database path
-    RUN_APPLICATION(ipAddress, "5566", logFilePath)
+    app = Application(
+        ipAddress = args.GetArg(utils.SupportedArgs.ipaddress),
+        logFilePath = args.GetArg(utils.SupportedArgs.logfilepath)
+    )
+    app.runServer()
--- Backend/utils.py
+++ Backend/utils.py
@@ -12,3 +12,43 @@ def nextRequestId(prefix : str):
     ids.append(nextId)
     __requestsIds__[prefix] = ids
     return "{}{:08x}".format(prefix, nextId)
+
+class SupportedArgs:
+    ipaddress = "ipaddress"
+    logfilepath = "logfilepath"
+
+class ArgvDeserializer:
+    argv = []
+    argsDict = {}
+
+    def __init__(self, argv : list):
+        self.argv = argv
+        self.deserialize()
+
+    def deserialize(self):
+        self.argsDict = dict()
+        idx : int = 1 # skip app name
+
+        while idx < len(self.argv):
+            arg : str = self.argv[idx]
+            
+            if arg.startswith('--'):
+                if self.IsNextArg(idx):
+                    nextArg : str = self.argv[idx + 1]
+                    if not nextArg.startswith('--'):
+                        self.argsDict[arg[2:]] = nextArg
+                        idx += 2
+                    else:
+                        self.argsDict[arg[2:]] = True
+                        idx += 1
+            idx += 1
+
+    def IsNextArg(self, index) -> str:
+        if index + 1 < len(self.argv):
+            return True
+
+    def GetArg(self, name):
+        if name in self.argsDict.keys():
+            return self.argsDict[name]
+        else:
+            return None
