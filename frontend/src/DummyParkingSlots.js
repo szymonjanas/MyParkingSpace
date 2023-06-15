b9df277b4cd57973200e7cb15adc2955f36ab59c
adrianbozko <gequpl@gmail.com>
2023-06-16 00:31:45 +0200

    [fe][tests] add dummy parking slots for manual tests during developement

---
+++ frontend/src/DummyParkingSlots.js
@@ -0,0 +1,82 @@
+export const DUMMY_PARKING_SLOTS = [
+    {
+        "ParkingSlotId": "PS1",
+        "SlotNumber": "A1",
+        "Floor": "1",
+        "PositionX": "2",
+        "PositionY": "4",
+        "isFree": true
+      },
+      {
+        "ParkingSlotId": "PS2",
+        "SlotNumber": "A2",
+        "Floor": "1",
+        "PositionX": "3",
+        "PositionY": "1",
+        "isFree": false
+      },
+      {
+        "ParkingSlotId": "PS3",
+        "SlotNumber": "B1",
+        "Floor": "1",
+        "PositionX": "5",
+        "PositionY": "5",
+        "isFree": true
+      },
+      {
+        "ParkingSlotId": "PS4",
+        "SlotNumber": "B2",
+        "Floor": "1",
+        "PositionX": "1",
+        "PositionY": "3",
+        "isFree": true
+      },
+      {
+        "ParkingSlotId": "PS5",
+        "SlotNumber": "C1",
+        "Floor": "2",
+        "PositionX": "4",
+        "PositionY": "2",
+        "isFree": false
+      },
+      {
+        "ParkingSlotId": "PS6",
+        "SlotNumber": "C2",
+        "Floor": "2",
+        "PositionX": "2",
+        "PositionY": "1",
+        "isFree": false
+      },
+      {
+        "ParkingSlotId": "PS7",
+        "SlotNumber": "D1",
+        "Floor": "2",
+        "PositionX": "5",
+        "PositionY": "4",
+        "isFree": true
+      },
+      {
+        "ParkingSlotId": "PS8",
+        "SlotNumber": "D2",
+        "Floor": "2",
+        "PositionX": "1",
+        "PositionY": "5",
+        "isFree": false
+      },
+      {
+        "ParkingSlotId": "PS9",
+        "SlotNumber": "E1",
+        "Floor": "3",
+        "PositionX": "3",
+        "PositionY": "2",
+        "isFree": true
+      },
+      {
+        "ParkingSlotId": "PS10",
+        "SlotNumber": "E2",
+        "Floor": "3",
+        "PositionX": "4",
+        "PositionY": "4",
+        "isFree": true
+      }
+]
